7ff71f80389511ce0a5c0a994b2a40345216854a
n00byN00byD0 <157893087+n00byN00byD0@users.noreply.github.com>
2024-01-27 19:38:59 +0000

    Update CoffinDecorChanger18.ts
    I'll live with my little bugbears and get out 'N' touch grass eventually.

--- CoffinDecorChanger18.ts
+++ CoffinDecorChanger18.ts
@@ -1,1 +1,20 @@
+/**
+* Copyright don't exist because the aim is to be off of the freshest maps!
+* If the map reflects the teritory the teritory reflects the map + a beating heart.
+* "The Lord gave you eyes! Plagerise!" -Liar's Poker Paraphrased.
+*     -on the same page, (ii) ©aptin, but what is that?
+*
+* KISS a ring 'N' don't take SH!T 
+*/
 
+// As renowned Dizzie Rascal stated:
+//   'Step into my zone, get your whole face rearranged.'.
+
+// TODO: Read bookmarks JSON, from the Firefox export functions output so that tabs
+// can have more room ... On the homepage.
+function extractFFBookmarksToStringMap(jsonOfUseful : string) {
+  let 
+}
+
+
+// TODO, TODO: Something else with more productive focus
